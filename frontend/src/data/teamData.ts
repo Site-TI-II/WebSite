640ab27f30b7fd3911da8dc1c19e3a9ac50f5761
Leonardo Araujo Passos <77520362+LeoPassos98@users.noreply.github.com>
2025-09-02 12:16:32 +0000

    14o commit, atualização dos membros da equipe.

--- frontend/src/data/teamData.ts
+++ frontend/src/data/teamData.ts
@@ -2,40 +2,40 @@ import { MembroEquipe } from '../types/team';
 
 export const membrosEquipe: MembroEquipe[] = [
     {
-      nome: "Ana Silva",
+      nome: "Caio Botelho",
       funcao: "Desenvolvedora Front-end",
-      contato: "ana.silva@messaway.com",
+      contato: "Caio.Botelho@messaway.com",
       foto: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80",
-      whatsapp: "+55 (11) 99999-9999",
-      github: "anadev",
-      linkedin: "ana-silva-dev"
+      whatsapp: "+55 (31) 8207-2834",
+      github: "caioDev",
+      linkedin: "caio-botelho-dev"
     },
     {
-      nome: "Carlos Oliveira",
-      funcao: "Desenvolvedor Back-end",
-      contato: "carlos.oliveira@messaway.com",
+      nome: "Danii",
+      funcao: "Desenvolvedora Back-end",
+      contato: "Danii@messaway.com",
       foto: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80",
-      whatsapp: "+55 (11) 98888-8888",
-      github: "carlosbackend",
-      linkedin: "carlos-oliveira-dev"
+      whatsapp: "+55 (31) 8438-4555",
+      github: "DaniiBackend",
+      linkedin: "Danii-oliveira-dev"
     },
     {
-      nome: "Mariana Santos",
+      nome: "Guilherme França",
       funcao: "Designer UX/UI",
-      contato: "mariana.santos@messaway.com",
+      contato: "Guilherme.franca@messaway.com",
       foto: "https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80",
-      whatsapp: "+55 (11) 97777-7777",
-      github: "maridesigner",
-      linkedin: "mariana-santos-ux"
+      whatsapp: "+55 (31) 99465-2311",
+      github: "ghsfranca",
+      linkedin: "ghsfranca"
     },
     {
-      nome: "Ricardo Ferreira",
-      funcao: "Gerente de Projeto",
-      contato: "ricardo.ferreira@messaway.com",
+      nome: "Leonardo Araujo",
+      funcao: "Desenvolvedora Front-end",
+      contato: "araujoleonardo5@gmail.com",
       foto: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&auto=format&fit=crop&w=400&q=80",
-      whatsapp: "+55 (11) 96666-6666",
-      github: "ricardopm",
-      linkedin: "ricardo-ferreira-pm"
+      whatsapp: "+55 (31) 99465-1693",
+      github: "LeoPassos98",
+      linkedin: "LeoPassos98"
     }
 ];
 
